2a3f17ade13334d49c7c477241fefe4f6f2b59f5
Mannycjr <manny@mycjr.com>
2021-07-29 23:53:06 -0700

    Completed modifications to Inventory script

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    // Let's pretend that Inventory script is done
+    
     // Start is called before the first frame update
     void Start()
     {
